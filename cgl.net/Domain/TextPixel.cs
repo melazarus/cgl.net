a30b403dac32401a6a5038184513452b7c5e4c88
Hans Polders <hans.polders@gmail.com>
2015-10-29 14:10:37 +0100

    TextPixel

---
+++ cgl.net/Domain/TextPixel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cgl.net
+{
+    public class TextPixel
+    {
+        /// <summary>
+        /// a null value means this property is transparrent
+        /// </summary>
+        public char? Character { get; }
+
+        /// <summary>
+        /// a null value means this property is transparrent
+        /// </summary>
+        public ConsoleColor? BackgroundColor { get; }
+
+        /// <summary>
+        /// a null value means this property is transparrent
+        /// </summary>
+        public ConsoleColor? ForegroundColor { get; }
+    }
+}
